98b2c3a27aaa32fc0c78d794690ad5ba1a330d80
Icarus <icaros1105@gmail.com>
2022-03-09 11:14:01 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperiro/dsmovie/controllers/ScoreController.java
@@ -0,0 +1,26 @@
+package com.devsuperiro.dsmovie.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperiro.dsmovie.dto.MovieDTO;
+import com.devsuperiro.dsmovie.dto.ScoreDTO;
+import com.devsuperiro.dsmovie.services.ScoreService;
+
+@RestController
+@RequestMapping(value = "/scores")
+public class ScoreController {
+	
+	@Autowired
+	private ScoreService service;
+	
+	@PutMapping
+	public MovieDTO saveScore(@RequestBody ScoreDTO dto){
+		MovieDTO movieDTO = service.saveScore(dto);
+		return movieDTO;
+	}
+	
+}
